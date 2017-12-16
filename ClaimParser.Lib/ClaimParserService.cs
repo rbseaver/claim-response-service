c64cbb8c170022894b6558289f5758dbb256deb2
Rob Seaver <rob.seaver@gmail.com>
2017-12-16 17:12:33 -0600

    Start parsing an actual claim -- start with small sample

--- ClaimParser.Lib/ClaimParserService.cs
+++ ClaimParser.Lib/ClaimParserService.cs
@@ -1,5 +1,8 @@
 ﻿using ClaimParser.Lib.Domain;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ClaimParser.Lib
 {
@@ -16,7 +19,28 @@ namespace ClaimParser.Lib
                 throw new ArgumentException("Text cannot be null or empty");
             }
 
-            return new PatentClaimDomain();
+            var model = new PatentClaimDomain
+            {
+                PatentClaims = new List<PatentClaim>()
+            };
+
+            const string splitPattern = @"\n?\d{1,}\.\s";
+            var claims = Regex.Split(claimText, splitPattern)
+                                .Where(c => c != string.Empty); // No empty lines.
+
+            var sequencer = 1;
+
+            foreach (var claim in claims)
+            {
+                model.PatentClaims.Add(new PatentClaim
+                {
+                    Sequence = sequencer,
+                    ClaimText = claim
+                });
+                sequencer++;
+            }
+
+            return model;
         }
     }
 }
--- ClaimParser.Tests/Unit/WhenParsingClaim.cs
+++ ClaimParser.Tests/Unit/WhenParsingClaim.cs
@@ -18,24 +18,47 @@ namespace ClaimParser.Tests.Unit
         [Test]
         public void AndParsingTextItShouldAcceptString()
         {
-            var service = new ClaimParserService();
+            var service = MakeService();
             Assert.DoesNotThrow(() => service.ParseClaims(ARandom.String(100)));
         }
 
         [Test]
         public void ItShouldThrowArgumentExceptionOnEmptyClaimText()
         {
-            var service = new ClaimParserService();
+            var service = MakeService(); ;
             Assert.Throws<ArgumentException>(() => service.ParseClaims(string.Empty));
         }
 
         [Test]
         public void AndParsingClaimItShouldConvertToDomainObject()
         {
-            var service = new ClaimParserService();
+            var service = MakeService();
             var patentClaim = service.ParseClaims(ARandom.String(100));
 
             Assert.That(patentClaim, Is.Not.Null);
+        }
+
+        [Test]
+        public void ItShouldPopulateObject()
+        {
+            var claimText = @"1. A product tag system, comprising:
+an RFID tag adapted for attachment to a product;
+at least one data store in said tag for bar code information relating to said product;
+a tag detacher for removing said tag from said product at a point of sale;
+an RFID tag reader for retrieving said bar code information from said tag when said tag is placed in said tag detacher; and,
+a display for presenting said bar code information in a form which can be scanned by a conventional bar code scanner, said display associated with said tag detacher and said RFID tag reader at said point of sale.
+2. The system of claim 1, wherein said tag further comprises a detectable EAS element.
+3. The system of claim 1, wherein said tag detacher is inoperable for detaching said tag until said bar code information has been read.
+4. The system of claim 1, wherein said tag further comprises a further data store for further product information.";
+            var service = MakeService();
+            var patentClaim = service.ParseClaims(claimText);
+
+            Assert.That(patentClaim.PatentClaims[0].Sequence, Is.EqualTo(1));
+        }
+
+        private static ClaimParserService MakeService()
+        {
+            return new ClaimParserService();
         }
     }
 }

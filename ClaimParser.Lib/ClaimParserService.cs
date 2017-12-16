cdcae80a68d67a8650a492f15ae71e20a2c1b932
Rob Seaver <rob.seaver@gmail.com>
2017-12-16 17:30:20 -0600

    Minor refactoring

--- ClaimParser.Lib/ClaimParserService.cs
+++ ClaimParser.Lib/ClaimParserService.cs
@@ -14,20 +14,22 @@ namespace ClaimParser.Lib
 
         public PatentClaimDomain ParseClaims(string claimText)
         {
-            if (string.IsNullOrEmpty(claimText))
-            {
-                throw new ArgumentException("Text cannot be null or empty");
-            }
+            CheckForEmpty(claimText);
 
             var model = new PatentClaimDomain
             {
                 PatentClaims = new List<PatentClaim>()
             };
 
-            const string splitPattern = @"\n?\d{1,}\.\s";
-            var claims = Regex.Split(claimText, splitPattern)
-                                .Where(c => c != string.Empty); // No empty lines.
+            var claims = ConvertToArray(claimText);
 
+            AddClaimsToModel(model, claims);
+
+            return model;
+        }
+
+        private static void AddClaimsToModel(PatentClaimDomain model, IEnumerable<string> claims)
+        {
             var sequencer = 1;
 
             foreach (var claim in claims)
@@ -39,8 +41,22 @@ namespace ClaimParser.Lib
                 });
                 sequencer++;
             }
+        }
 
-            return model;
+        private static void CheckForEmpty(string claimText)
+        {
+            if (string.IsNullOrEmpty(claimText))
+            {
+                throw new ArgumentException("Text cannot be null or empty");
+            }
+        }
+
+        private static IEnumerable<string> ConvertToArray(string claimText)
+        {
+            const string splitPattern = @"\n?\d{1,}\.\s";
+            var claims = Regex.Split(claimText, splitPattern)
+                                .Where(c => c != string.Empty).ToArray(); // No empty lines.
+            return claims;
         }
     }
 }

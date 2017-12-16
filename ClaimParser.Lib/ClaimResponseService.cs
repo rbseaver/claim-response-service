080cad962c2537847d484000ce5bf52fbdb31879
Rob Seaver <rob.seaver@gmail.com>
2017-12-16 17:34:59 -0600

    Start claim response service

---
+++ ClaimParser.Lib/ClaimResponseService.cs
@@ -0,0 +1,12 @@
+﻿namespace ClaimParser.Lib
+{
+    public class ClaimResponseService
+    {
+        private ClaimParserService _claimParserService;
+
+        public ClaimResponseService(ClaimParserService claimParserService)
+        {
+            _claimParserService = claimParserService;
+        }
+    }
+}
---
+++ ClaimParser.Tests/Unit/WhenGeneratingResponse.cs
@@ -0,0 +1,20 @@
+﻿using ClaimParser.Lib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaimParser.Tests.Unit
+{
+    [TestFixture]
+    public class WhenGeneratingResponse
+    {
+        [Test]
+        public void ItShouldInitializeServiceWithoutError()
+        {
+            Assert.DoesNotThrow(() => new ClaimResponseService(new ClaimParserService()));
+        }
+    }
+}

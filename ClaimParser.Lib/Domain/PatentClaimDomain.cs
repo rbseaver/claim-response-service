3e18a25ba32690daa6df88039cd1775323117c6f
Rob Seaver <rob.seaver@gmail.com>
2017-12-16 16:27:42 -0600

    Build out domain object

--- ClaimParser.Lib/Domain/PatentClaimDomain.cs
+++ ClaimParser.Lib/Domain/PatentClaimDomain.cs
@@ -1,6 +1,15 @@
-﻿namespace ClaimParser.Lib.Domain
+﻿using System.Collections.Generic;
+
+namespace ClaimParser.Lib.Domain
 {
     public class PatentClaimDomain
     {
+        public IEnumerable<PatentClaim> PatentClaims { get; set; }
+    }
+
+    public class PatentClaim
+    {
+        public int Sequence { get; set; }
+        public string ClaimText { get; set; }
     }
 }
